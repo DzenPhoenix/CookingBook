f24f75e429466132e6c8ebad33c3ce1fab9e06f7
Denis Maksimenko <DzenPhoenix@gmail.com>
2020-09-28 14:13:55 +0300

    Add classes Recipe,Category,Kitchen

---
+++ DataLayer/Models/Kitchen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookingBook.DataLayer.Models
+{ 
+    
+    public class Kitchen
+    {
+        public int KitchenId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Recipe> Recipes { get; set; }
+        public Kitchen() { this.Recipes = new List<Recipe>(); }
+    }
+}
---
+++ DataLayer/Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookingBook.DataLayer.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string SerializedIngridients { get; set; }
+        public string SerializedInstructions { get; set; }
+        public int? KitchenId { get; set; }
+        public Kitchen Kitchen { get; set; }
+    }
+
+}
